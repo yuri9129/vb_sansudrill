01c4ed43807ba72dbd0910cd5acfb09e39de86e5
hayashi <hiroki-hayashi-2@alpsgiken.co.jp>
2017-05-19 15:36:46 +0900

    入力フォームと出力フォームを作成

--- sansudrill/Form1.Designer.vb
+++ sansudrill/Form1.Designer.vb
@@ -22,9 +22,54 @@ Partial Class Form1
     'コード エディターを使って変更しないでください。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.InBox = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.OutBox = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'InBox
+        '
+        Me.InBox.Location = New System.Drawing.Point(63, 255)
+        Me.InBox.Name = "InBox"
+        Me.InBox.Size = New System.Drawing.Size(80, 19)
+        Me.InBox.TabIndex = 0
+        Me.InBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("MS UI Gothic", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 255)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(45, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "答え"
+        '
+        'OutBox
+        '
+        Me.OutBox.Location = New System.Drawing.Point(169, 89)
+        Me.OutBox.Multiline = True
+        Me.OutBox.Name = "OutBox"
+        Me.OutBox.ReadOnly = True
+        Me.OutBox.Size = New System.Drawing.Size(130, 185)
+        Me.OutBox.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(321, 307)
+        Me.Controls.Add(Me.OutBox)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.InBox)
+        Me.Name = "Form1"
+        Me.Text = "算数ドリル"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents InBox As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents OutBox As TextBox
 End Class
--- sansudrill/Form1.vb
+++ sansudrill/Form1.vb
@@ -1,3 +1,14 @@
 ﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub InBox_KeyPress(sender As Object, e As KeyPressEventArgs) Handles InBox.KeyPress
+        If e.KeyChar = Chr(Keys.Enter) Then
+            Console.WriteLine(InBox.Text & "が入力されました。")
+            OutBox.AppendText(InBox.Text & vbCrLf)
+            InBox.Text = ""
+        End If
+    End Sub
 
 End Class
